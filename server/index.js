cfc5c9f243ddd201921e52cad067c3a2f2d945b2
Ben Cronin <benc@bens-mbp.lan>
2018-07-25 09:03:53 -0700

    Allow cross-orgin requests

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 3001;
 const app = express();
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 app.get('/api/searchRecords', (req, res) => {
   model.getSearchRecords((err, results) => {
@@ -26,6 +31,7 @@ app.get('/api/searchListings/:searchQuery', (req, res) => {
 
 app.post('/api/searchRecords', (req, res) => {
   const { searchQuery } = req.body;
+  res.header('Access-Control-Allow-Origin', '*');
   model.postSearchRecord(searchQuery, (err, results) => {
     res.statusCode = err ? 400 : 200;
     res.send(err || results);
