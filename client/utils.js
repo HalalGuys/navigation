95dc38a90459f2538bf4ed059a0d237482e1fd3c
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-01 14:45:17 -0700

    Add port to api domain url

--- client/utils.js
+++ client/utils.js
@@ -1,6 +1,6 @@
 const apiDomain = window.location.href.includes('localhost')
   ? 'http://localhost:2999'
-  : 'http://ec2-34-217-69-244.us-west-2.compute.amazonaws.com';
+  : 'http://ec2-34-217-69-244.us-west-2.compute.amazonaws.com:2999';
 
 const postRecordsEndpoint = `${apiDomain}/api/searchRecords`;
 const getRecordsEndpoint = `${apiDomain}/api/searchRecords`;
