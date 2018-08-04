c3b175717375593db561fd20b5a46f8f252bccad
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-04 15:12:39 -0700

    Refactor to conditionally render component based on DOM structure

--- client/index.jsx
+++ client/index.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import Landing from './Components/Landing/Landing';
 import Results from './Components/Results/Results';
 import Navbar from './Components/Navbar/Navbar';
@@ -5,3 +9,34 @@ import Navbar from './Components/Navbar/Navbar';
 window.Landing = Landing;
 window.Results = Results;
 window.Navbar = Navbar;
+
+const container = document.getElementById('Navigation');
+
+const Navigation = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route
+        path="/search/:searchQuery"
+        render={props => (
+          <div>
+            <Navbar {...props} />
+            <Results {...props} />
+          </div>
+        )}
+      />
+      <Route
+        path="/listing/:listingId"
+        render={props => (
+          <div>
+            <Navbar {...props} />
+          </div>
+        )}
+      />
+    </div>
+  </BrowserRouter>
+);
+
+if (container) {
+  ReactDOM.render(<Navigation />, container);
+}
