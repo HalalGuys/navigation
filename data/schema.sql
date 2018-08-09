497a07e253be80e82d19382daa8e054b75102a2e
Chris W <christopherkwildenradt@gmail.com>
2018-08-09 11:52:08 -0700

    Change data types, lower varchar limits

--- data/schema.sql
+++ data/schema.sql
@@ -5,10 +5,10 @@ CREATE DATABASE listings;
 
 CREATE TABLE searchListing (
     listingId SERIAL,
-    title VARCHAR(50) NOT NULL,
-    host VARCHAR(50) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    photoURL VARCHAR(200) NOT NULL
+    title TEXT NOT NULL,
+    host TEXT NOT NULL,
+    city TEXT NOT NULL,
+    photoURL VARCHAR(70) NOT NULL
 );
 
 CREATE TABLE searchRecord (
