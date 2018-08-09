de01f22714f28bf910f657b51f44d2770b7ddcf0
Chris W <christopherkwildenradt@gmail.com>
2018-08-09 12:59:11 -0700

    Switch routes to work with Postgres setup

--- server/index.js
+++ server/index.js
@@ -12,15 +12,16 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api/searchRecords', (req, res) => {
-  model.getSearchRecords((err, results) => {
-    if (err) console.log(err);
-    res.statusCode = err ? 400 : 200;
-    res.send(err || results);
-  });
-});
+// app.get('/api/searchRecords', (req, res) => {
+//   model.getSearchRecords((err, results) => {
+//     if (err) console.log(err);
+//     res.statusCode = err ? 400 : 200;
+//     res.send(err || results);
+//   });
+// });
 
 app.get('/api/searchListings/:searchQuery', (req, res) => {
+  console.log('search listings get request received', req.params)
   const { searchQuery } = req.params;
   model.getSearchResults(searchQuery, (err, results) => {
     if (err) console.log(err);
@@ -29,29 +30,31 @@ app.get('/api/searchListings/:searchQuery', (req, res) => {
   });
 });
 
-app.post('/api/searchRecords', (req, res) => {
-  const { searchQuery } = req.body;
-  res.header('Access-Control-Allow-Origin', '*');
-  model.postSearchRecord(searchQuery, (err, results) => {
-    if (err) console.log(err);
-    res.statusCode = err ? 400 : 200;
-    res.send(err || results);
-  });
-});
+// app.post('/api/searchRecords', (req, res) => {
+//   const { searchQuery } = req.body;
+//   res.header('Access-Control-Allow-Origin', '*');
+//   model.postSearchRecord(searchQuery, (err, results) => {
+//     if (err) console.log(err);
+//     res.statusCode = err ? 400 : 200;
+//     res.send(err || results);
+//   });
+// });
 
-app.put('/api/searchListings/:listingId', (req, res) => {
-  model.updateSearchListing(req.params.listingId, req.body, (err, results) => {
-    if (err) console.log(err);
-    res.send(results);
-  });
-});
+// app.put('/api/searchListings/:listingId', (req, res) => {
+//   model.updateSearchListing(req.params.listingId, req.body, (err, results) => {
+//     if (err) console.log(err);
+//     res.statusCode = err ? 400 : 200;
+//     res.send(err || results);
+//   });
+// });
 
-app.delete('/api/searchListings/:listingId', (req, res) => {
-  model.deleteSearchListing(req.params.listingId, (err, results) => {
-    if (err) console.log(err);
-    res.send(results);
-  });
-});
+// app.delete('/api/searchListings/:listingId', (req, res) => {
+//   model.deleteSearchListing(req.params.listingId, (err, results) => {
+//     if (err) console.log(err);
+//     res.statusCode = err ? 400 : 200;
+//     res.send(err || results);
+//   });
+// });
 
 app.use('/*', express.static(`${__dirname}/../public`));
 
--- server/model.js
+++ server/model.js
@@ -1,55 +1,68 @@
-const models = require('../data');
 
-const getSearchResults = function (searchQuery, callback) {
-  models.SearchListing.find({
-    $or: [{ title: { $regex: searchQuery } }, { city: { $regex: searchQuery } }],
-  })
-    .then((results) => {
-      callback(null, results);
+require('dotenv').config()
+const pg = require('pg');
+const format = require('pg-format');
+
+
+const config = {
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+}
+
+const pool = new pg.Pool(config);
+let myClient;
+
+const getSearchResults = function (searchTerm, cb) {
+  console.log('getsearch results called', searchTerm)
+    pool.connect((err, client) => {
+      myClient = client;
+      const query = format('select * from searchListing where listingId < 1000;')
+        myClient.query(query, (err, result) => {
+          if (err) console.log(query, err);
+          cb(null, result.rows)
+        })
     })
-    .catch((err) => {
-      callback(err);
-    });
-};
-
-const getSearchRecords = function (callback) {
-  models.SearchRecord.find({})
-    .sort('-createdAt')
-    .exec(callback);
-};
-
-const postSearchRecord = function (searchQuery, callback) {
-  const searchRecord = new models.SearchRecord({ text: searchQuery, createdAt: new Date() });
-  searchRecord
-    .save()
-    .then(results => callback(null, results))
-    .catch(err => callback(err));
-};
-
-const updateSearchListing = (id, data, cb) => {
-  models.SearchListing.findByIdAndUpdate(id, data, {new: true}, (err, result) => {
-    if (err) console.log(err);
-    return cb(null, result);
-  })
 }
 
-const deleteSearchListing = (id, cb) => {
-  models.SearchListing.findByIdAndRemove(id, (err, results) => {  
-    if (err) console.log(err);
 
-    const response = {
-        message: "Successfully deleted",
-        id: id
-    };
-    return cb(null, response)
-  });
-}
+// const getSearchRecords = function (callback) {
+//   models.SearchRecord.find({})
+//     .sort('-createdAt')
+//     .exec(callback);
+// };
+
+// const postSearchRecord = function (searchQuery, callback) {
+//   const searchRecord = new models.SearchRecord({ text: searchQuery, createdAt: new Date() });
+//   searchRecord
+//     .save()
+//     .then(results => callback(null, results))
+//     .catch(err => callback(err));
+// };
+
+// const updateSearchListing = (id, data, cb) => {
+//   models.SearchListing.findByIdAndUpdate(id, data, {new: true}, (err, result) => {
+//     if (err) console.log(err);
+//     return cb(null, result);
+//   })
+// }
+
+// const deleteSearchListing = (id, cb) => {
+//   models.SearchListing.findByIdAndRemove(id, (err, results) => {  
+//     if (err) console.log(err);
+
+//     const response = {
+//         message: "Successfully deleted",
+//         id: id
+//     };
+//     return cb(null, response)
+//   });
+// }
 
 
 module.exports = {
   getSearchResults,
-  getSearchRecords,
-  postSearchRecord,
-  updateSearchListing,
-  deleteSearchListing,
+  // getSearchRecords,
+  // postSearchRecord,
+  // updateSearchListing,
+  // deleteSearchListing,
 };
